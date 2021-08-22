190c794b20e8d28cf805fbf6ca54c083ed488054
terada0628 <ntpeanuts@yahoo.co.jp>
2021-08-22 11:23:07 +0000

    モデルテスト済

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :contact do
+    inquiry                {"商品やお店についてのお問い合わせ"}
+    title                 {"8/15の19時に届いた商品について"}
+    content               {"焼きおにぎりを6個注文したのだが、5個しか入ってなかった"}
+    email                 {"test@gmail.com"}
+    family_name           {"大野"}
+    first_name            {"智"}
+    family_name_kana      {"オオノ"}
+    first_name_kana       {"サトシ"}
+    sex                   {"男性"}
+    postal_code           {"0002222"}
+    address               {"東京都練馬区"}
+    telephone_number      {"77788889999"}
+
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,13 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-format documentation
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'support/factory_bot'
+require 'devise'
+
+# require 'support/factory_bot'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -22,7 +23,16 @@ require 'support/factory_bot'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+ Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+# require Fire.expand_path("spec/support/controller_macros.rb")
+
+
+# 26行目のコード移行に記述しないと読み込まない↓↓↓
+  RSpec.configure do |config|
+    config.include ControllerMacros # 追記
+  end
+  require_relative 'support/controller_macros'
+  
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -32,9 +42,22 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
+
+  # ログインヘルパーの準備
+  # config.include Devise::Test::IntegrationHelpers, type: :request #sign_inヘルパーを提供してくれます
+  # config.include FactoryBot::Syntax::Methods #ついでにFactoryBotもincludeしておきます
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.extend ControllerMacros, :type => :controller
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.include FactoryBot::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
