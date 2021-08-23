2cbb3c55277729cd1668a16cf5f050789fda6f7d
terada0628 <ntpeanuts@yahoo.co.jp>
2021-08-23 01:16:16 +0000

    test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require 'rspec/rails'
 require 'devise'
 
 # require 'support/factory_bot'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
